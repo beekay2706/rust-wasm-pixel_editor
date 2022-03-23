a8a7bbfcd972a4a51d3c64ad719a78ab082b109d
beekay2706 <93442895+beekay2706@users.noreply.github.com>
2022-03-23 16:21:20 +0530

    error check

--- editor/js/index.js
+++ editor/js/index.js
@@ -1,9 +1,41 @@
-async function main(){
+function draw(image){
+   const canvas = document.getElementById("my-canvas");
+   const context = canvas.getContext("2d");
+
+//    context.fillStyle = "red";
+//    context.fillRect(0,0, 150,150);
+context.strokeStyle = "black";
+context.lineWidth = 1;
+
+const width = image.width();
+const height = image.height();
+const cellSize = 50;
+
+// const cells = image.cells();
+
+
+for(let x = 0 ; x <= width; x++){
+context.beginPath();
+context.moveTo(x*cellSize + 0.5, 0);
+context.lineTo(x*cellSize + 0.5, height*cellSize);
+context.stroke();
+}
+for(let y = 0 ; y <= height; y++){
+    context.beginPath();
+    context.moveTo(0, y * cellSize + 0.5);
+    context.lineTo(width*cellSize, y * cellSize + 0.5);
+    context.stroke();
+    }
+}
+
+async function main()
+{
 const lib = await import("../pkg/index.js").catch(console.error);
 
-const image = lib.image.new();
+const image = new lib.Image(10, 10);
 
 console.log(image);
+draw(image);
 }
 
 main();
--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -10,13 +10,56 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+// #[derive(Clone, Copy)]
+// pub struct Rgb {
+//     r:u8,
+//     g:u8,
+//     b:u8,
+// }
+#[wasm_bindgen(constructor)]
+ pub struct Image {
+    width: usize,
+    height: usize,
+}
 #[wasm_bindgen]
-pub struct image {
+impl Image {
+    #[wasm_bindgen(constructor)]
+   pub fn new(width: usize, height: usize) -> Image {
+       Image{width, height}
+   } 
+   pub fn width(&self) -> usize {
+    self.width
+}
+pub fn height(&self) -> usize {
+    self.height
+}
 
-}
-#[wasm_bindgen]
-impl image {
-    fn new() -> image {
-        image{}
-    }
-}
+} 
+// #[wasm_bindgen]
+// impl Imag e {
+//     #[wasm_bindgen(constructor)]
+//    pub fn new(width: usize, height: usize) -> Image {
+//     //    let mut cells = Vec::new();
+//     //    cells.resize(
+//     //        width * height,
+//     //         Rgb{
+//     //            r:200,
+//     //            g:200,
+//     //            b:255,
+//     //        },
+//     //    );
+//         Image{ 
+//             width, 
+//             height,
+//             // cells
+//          }
+//     // }
+//     // pub fn cells(&self) -> Vec<u8> {
+//     //     self.cells
+//     //     .iter()
+//     //     .map(|&rgb| vec![rgb.r, rgb.g, rgb.b])
+//     //     .collect::<Vec<Vec<u8>>>()
+//     //     .concat()
+//      }
+    
+// }
