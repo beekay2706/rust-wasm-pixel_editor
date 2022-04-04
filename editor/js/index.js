cc14ff90330e2445cc674079e98cef5bfd422be5
beekay2706 <93442895+beekay2706@users.noreply.github.com>
2022-04-04 20:39:54 +0530

    update color profiles

--- editor/js/index.js
+++ editor/js/index.js
@@ -1,18 +1,28 @@
-function draw(image){
+const cellSize = 50;
+
+function draw(state){
    const canvas = document.getElementById("my-canvas");
    const context = canvas.getContext("2d");
 
-//    context.fillStyle = "red";
-//    context.fillRect(0,0, 150,150);
+  //  context.fillStyle = "white";
+  //  context.fillRect(0, 150,150,0);
 context.strokeStyle = "black";
 context.lineWidth = 1;
 
+const image = state.image;
 const width = image.width();
 const height = image.height();
-const cellSize = 50;
 
-// const cells = image.cells();
 
+const cells = image.cells();
+
+for(let x = 0 ; x < width; x++){
+    for(let y = 0 ; y < height; y++){ 
+      const index = ((y * width) + x) * 3;
+      const color = "rgb(${cells[index + 0]}, ${cells[index + 1]}, ${cells[index + 2]})";
+      context.fillStyle = state.currentColor;
+      context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+    } }
 
 for(let x = 0 ; x <= width; x++){
 context.beginPath();
@@ -28,14 +38,46 @@ for(let y = 0 ; y <= height; y++){
     }
 }
 
+Document.getElementById("red").addEventListener("click", (event) => {
+        state.currentColor = [255, 200, 200]
+});
+Document.getElementById("blue").addEventListener("click", (event) => {
+  state.currentColor = [200, 200, 255]
+});
+Document.getElementById("green").addEventListener("click", (event) => {
+  state.currentColor = [200, 255, 200]
+});
+function setupCanvas(image) {
+ const canvas = document.getElementById("my-canvas");
+
+ canvas.addEventListener('click', (event)=> {
+  const rect = canvas.getBoundingClientRect();
+
+  let x = event.clientX - rect.left;
+  let y = event.clientY - rect.top;
+
+  x = Math.floor(x / cellSize);
+  y = Math.floor(y / cellSize);
+
+  const image = state.image;
+   image.brush(x, y,state.currentColor);
+  draw(image);
+ })
+
+}
+ 
 async function main()
 {
 const lib = await import("../pkg/index.js").catch(console.error);
 
 const image = new lib.Image(10, 10);
 
-console.log(image);
-draw(image);
+const state = {
+  image,
+  currentColor: [200, 255, 200]
+}
+ setupCanvas(state);
+draw(state);
 }
 
 main();
--- editor/src/lib.rs
+++ editor/src/lib.rs
@@ -10,56 +10,60 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-// #[derive(Clone, Copy)]
-// pub struct Rgb {
-//     r:u8,
-//     g:u8,
-//     b:u8,
-// }
+#[derive(Clone, Copy)]
+pub struct Rgb {
+    r:u8,
+    g:u8,
+    b:u8,
+}
 #[wasm_bindgen(constructor)]
  pub struct Image {
     width: usize,
     height: usize,
+    cells: Vec<Rgb>
 }
 #[wasm_bindgen]
 impl Image {
     #[wasm_bindgen(constructor)]
    pub fn new(width: usize, height: usize) -> Image {
-       Image{width, height}
+    let mut cells = Vec::new();
+           cells.resize(
+               width * height,
+                Rgb{
+                   r:200,
+                   g:200,
+                   b:255,
+               },
+           );
+       Image{
+        width, 
+        height,
+        cells,
+    }
    } 
+    
    pub fn width(&self) -> usize {
     self.width
 }
 pub fn height(&self) -> usize {
     self.height
 }
+pub fn cells(&self) -> Vec<u8> {
+    self.cells
+    .iter()
+    .map(|&rgb| vec![rgb.r, rgb.g, rgb.b])
+    .collect::<Vec<Vec<u8>>>()
+    .concat()
+ }
+ pub fn brush(&mut self, x: usize, y: usize, color:Vec<u8>)
+ {
+   let index = (y * self.width) + x;
+   self.cells[index] = Rgb {
+   r: color[0],
+   g: color[1],
+   b: color[2]
+   }
+ }
 
 } 
-// #[wasm_bindgen]
-// impl Imag e {
-//     #[wasm_bindgen(constructor)]
-//    pub fn new(width: usize, height: usize) -> Image {
-//     //    let mut cells = Vec::new();
-//     //    cells.resize(
-//     //        width * height,
-//     //         Rgb{
-//     //            r:200,
-//     //            g:200,
-//     //            b:255,
-//     //        },
-//     //    );
-//         Image{ 
-//             width, 
-//             height,
-//             // cells
-//          }
-//     // }
-//     // pub fn cells(&self) -> Vec<u8> {
-//     //     self.cells
-//     //     .iter()
-//     //     .map(|&rgb| vec![rgb.r, rgb.g, rgb.b])
-//     //     .collect::<Vec<Vec<u8>>>()
-//     //     .concat()
-//      }
-    
-// }
+
